733a1f5aa1d38d3c03b72fa753750f5690d6b372
Ana R <ana.rdrg@gmail.com>
2018-11-20 14:21:19 +0000

    Add more questions to work start/complete;

--- planning-app/src/forms/works/WorkStart.vue
+++ planning-app/src/forms/works/WorkStart.vue
@@ -24,23 +24,116 @@
 			</fieldset>
 		</div>
 
-		<div v-if="hasWorkStarted">
-			<div class="govuk-warning-text">
-				<span class="govuk-warning-text__icon" aria-hidden="true">!</span>
-				<strong class="govuk-warning-text__text">
-					<span class="govuk-warning-text__assistive">Warning</span>
-					Message to applicant
-				</strong>
-			</div>
-		</div>
+		<div v-if="hasWorkStarted" class="govuk-inset-text">
+			<warning-message :message="warningMessage" v-bind:typeAlert="false"></warning-message>
 
-		<v-text-area 
-			label="Describe what you have already done" 
-			v-model="workDetails" 
-			name="more-detail" 
-			v-if="hasWorkStarted" 
-			:isRequired="hasWorkStarted">
-		</v-text-area>
+			<legend class="govuk-fieldset__legend govuk-fieldset__legend--xl">
+				<h4 class="govuk-heading-s">
+					When did the work start?
+				</h4>
+			</legend>
+
+			<div class="govuk-form-group">
+				<fieldset class="govuk-fieldset" aria-describedby="dob-hint" role="group">
+					<div class="govuk-date-input" id="dob">
+						<div class="govuk-date-input__item">
+							<div class="govuk-form-group">
+								<label class="govuk-label govuk-date-input__label" for="dob-day">
+									Day
+								</label>
+								<input class="govuk-input govuk-date-input__input govuk-input--width-2" id="dob-day" name="dob-day" type="number" pattern="[0-9]*">
+							</div>
+						</div>
+						<div class="govuk-date-input__item">
+							<div class="govuk-form-group">
+								<label class="govuk-label govuk-date-input__label" for="dob-month">
+									Month
+								</label>
+								<input class="govuk-input govuk-date-input__input govuk-input--width-2" id="dob-month" name="dob-month" type="number" pattern="[0-9]*">
+							</div>
+						</div>
+						<div class="govuk-date-input__item">
+							<div class="govuk-form-group">
+								<label class="govuk-label govuk-date-input__label" for="dob-year">
+									Year
+								</label>
+								<input class="govuk-input govuk-date-input__input govuk-input--width-4" id="dob-year" name="dob-year" type="number" pattern="[0-9]*">
+							</div>
+						</div>
+					</div>
+				</fieldset>
+			</div>
+
+			<div class="govuk-form-group">
+				<fieldset class="govuk-fieldset" aria-describedby="work-completed-hint">
+					<legend class="govuk-fieldset__legend govuk-fieldset__legend--xl">
+					<h4 class="govuk-heading-s">
+						Has the work already been completed?
+					</h4>
+					</legend>
+					<div class="govuk-radios govuk-radios--inline">
+						<div class="govuk-radios__item">
+							<input class="govuk-radios__input" id="work-completed-1" name="work-completed" type="radio" value="Yes" v-model="workCompleted">
+							<label class="govuk-label govuk-radios__label" for="work-completed-1">
+								Yes
+							</label>
+						</div>
+						<div class="govuk-radios__item">
+							<input class="govuk-radios__input" id="work-completed-2" name="work-completed" type="radio" value="No" v-model="workCompleted">
+							<label class="govuk-label govuk-radios__label" for="work-completed-2">
+								No
+							</label>
+						</div>
+					</div>
+				</fieldset>
+			</div>
+
+			<div v-if="isWorkComplete" class="govuk-inset-text">
+				<legend class="govuk-fieldset__legend govuk-fieldset__legend--xl">
+					<h4 class="govuk-heading-s">
+						When did the work finish?
+					</h4>
+				</legend>
+
+				<div class="govuk-form-group">
+					<fieldset class="govuk-fieldset" aria-describedby="dob-hint" role="group">
+						<div class="govuk-date-input" id="dob">
+							<div class="govuk-date-input__item">
+								<div class="govuk-form-group">
+									<label class="govuk-label govuk-date-input__label" for="dob-day">
+										Day
+									</label>
+									<input class="govuk-input govuk-date-input__input govuk-input--width-2" id="dob-day" name="dob-day" type="number" pattern="[0-9]*">
+								</div>
+							</div>
+							<div class="govuk-date-input__item">
+								<div class="govuk-form-group">
+									<label class="govuk-label govuk-date-input__label" for="dob-month">
+										Month
+									</label>
+									<input class="govuk-input govuk-date-input__input govuk-input--width-2" id="dob-month" name="dob-month" type="number" pattern="[0-9]*">
+								</div>
+							</div>
+							<div class="govuk-date-input__item">
+								<div class="govuk-form-group">
+									<label class="govuk-label govuk-date-input__label" for="dob-year">
+										Year
+									</label>
+									<input class="govuk-input govuk-date-input__input govuk-input--width-4" id="dob-year" name="dob-year" type="number" pattern="[0-9]*">
+								</div>
+							</div>
+						</div>
+					</fieldset>
+				</div>
+			</div>
+
+			<v-text-area 
+				label="Describe what you have already done" 
+				v-model="workDetails" 
+				name="more-detail" 
+				:isRequired="hasWorkStarted">
+			</v-text-area>
+		</div>
 
 		<v-cta name="Next" :onClick="navigate"></v-cta>
 	</div>
@@ -50,6 +143,7 @@
 import vCta from '../../components/Cta.vue';
 import vTextArea from '../../components/form/vTextArea.vue';
 import router from '../../router';
+import WarningMessage from '../../components/WarningMessage.vue';
 
 export default {
 	name: 'WorkStart',
@@ -61,7 +155,9 @@ export default {
     return {
 			question: 'Has the work already been started?',
 			workStarted: undefined,
-			workDetails: undefined
+			workDetails: undefined,
+			warningMessage: 'Message to applicant.',
+			workCompleted: undefined
     }
   },
 	methods: {
@@ -105,6 +201,9 @@ export default {
 	computed: {
 		hasWorkStarted () {
 			return this.workStarted === 'Yes';
+		},
+		isWorkComplete () {
+			return this.workCompleted === 'Yes';
 		}
 	}
 }
