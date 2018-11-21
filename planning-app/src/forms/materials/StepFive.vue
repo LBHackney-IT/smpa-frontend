a53fdd5f7a99afdab78b724f0dbdbfa30a27c220
Ana R <ana.rdrg@gmail.com>
2018-11-21 15:43:49 +0000

    Fix Redirect after step 5;

--- planning-app/src/forms/materials/StepFive.vue
+++ planning-app/src/forms/materials/StepFive.vue
@@ -73,7 +73,7 @@ export default {
   },
 	methods: {
     navigate() {
-      router.push({ name: 'Documentation' });
+      router.push({ name: 'SupportingDocumentation' });
     }
   },
   computed: {
