aceb1978664c8c9c17e8c30914ed79d84ae7e547
Ana R <ana.rdrg@gmail.com>
2018-11-22 11:19:10 +0000

    Changes from vue-cli-plugin-s3-deploy;

--- planning-app/vue.config.js
+++ planning-app/vue.config.js
@@ -3,5 +3,23 @@ module.exports = {
         extract: { 
             filename: "styles.css" 
         }
-    } 
+    },
+
+    pluginOptions: {
+      s3Deploy: {
+        awsProfile: 'default',
+        region: 'eu-west-2',
+        bucket: 'smpa.hactar.is',
+        createBucket: false,
+        staticHosting: false,
+        assetPath: 'dist',
+        assetMatch: '**',
+        deployPath: '/',
+        acl: 'public-read',
+        pwa: false,
+        enableCloudfront: false,
+        uploadConcurrency: 5,
+        pluginVersion: '3.0.0'
+      }
+    }
 }
