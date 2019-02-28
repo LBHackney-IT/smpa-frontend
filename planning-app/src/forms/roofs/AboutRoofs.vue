9619f363e25f990544b27f107e1d6ea884ba1749
Ana R <ana.rdrg@gmail.com>
2019-02-28 14:20:38 +0000

    Update values of roof selections;

--- planning-app/src/forms/roofs/AboutRoofs.vue
+++ planning-app/src/forms/roofs/AboutRoofs.vue
@@ -17,7 +17,7 @@
 
         <div class="govuk-checkboxes">
           <div class="govuk-checkboxes__item">
-            <input class="govuk-checkboxes__input" id="proposal-1" name="proposal" type="checkbox" value="Single storey extension" v-model="selectedProposal">
+            <input class="govuk-checkboxes__input" id="proposal-1" name="proposal" type="checkbox" value="Addition of a dormer extension" v-model="selectedProposal">
             <label class="govuk-label govuk-checkboxes__label" for="proposal-1">
               <strong>Addition of a dormer extension</strong>
             </label>
@@ -25,56 +25,56 @@
 
 
           <div class="govuk-checkboxes__item">
-            <input class="govuk-checkboxes__input" id="proposal-2" name="proposal" type="checkbox" value="Two storey extension" v-model="selectedProposal">
+            <input class="govuk-checkboxes__input" id="proposal-2" name="proposal" type="checkbox" value="Removal of a dormer extension" v-model="selectedProposal">
             <label class="govuk-label govuk-checkboxes__label" for="proposal-2">
               <strong>Removal of a dormer extension</strong>
             </label>
           </div>
 
           <div class="govuk-checkboxes__item">
-            <input class="govuk-checkboxes__input" id="proposal-3" name="proposal" type="checkbox" value="Basement works" v-model="selectedProposal">
+            <input class="govuk-checkboxes__input" id="proposal-3" name="proposal" type="checkbox" value="Creation of a mansard styled roof extension" v-model="selectedProposal">
             <label class="govuk-label govuk-checkboxes__label" for="proposal-3">
               <strong>Creation of a mansard styled roof extension</strong>
             </label>
           </div>
 
           <div class="govuk-checkboxes__item">
-            <input class="govuk-checkboxes__input" id="proposal-4" name="proposal" type="checkbox" value="Basement works" v-model="selectedProposal">
+            <input class="govuk-checkboxes__input" id="proposal-4" name="proposal" type="checkbox" value="Installation of rooflight(s) and/or roof lantern(s)" v-model="selectedProposal">
             <label class="govuk-label govuk-checkboxes__label" for="proposal-4">
               <strong>Installation of rooflight(s) and/or roof lantern(s)</strong>
             </label>
           </div>
 
           <div class="govuk-checkboxes__item">
-            <input class="govuk-checkboxes__input" id="proposal-5" name="proposal" type="checkbox" value="Basement works" v-model="selectedProposal">
+            <input class="govuk-checkboxes__input" id="proposal-5" name="proposal" type="checkbox" value="Addition of a new storey(s)" v-model="selectedProposal">
             <label class="govuk-label govuk-checkboxes__label" for="proposal-5">
               <strong>Addition of a new storey(s)</strong>
             </label>
           </div>
 
           <div class="govuk-checkboxes__item">
-            <input class="govuk-checkboxes__input" id="proposal-6" name="proposal" type="checkbox" value="Basement works" v-model="selectedProposal">
+            <input class="govuk-checkboxes__input" id="proposal-6" name="proposal" type="checkbox" value="Alteration of a roof slope" v-model="selectedProposal">
             <label class="govuk-label govuk-checkboxes__label" for="proposal-6">
               <strong>Alteration of a roof slope</strong>
             </label>
           </div>
 
           <div class="govuk-checkboxes__item">
-            <input class="govuk-checkboxes__input" id="proposal-7" name="proposal" type="checkbox" value="Basement works" v-model="selectedProposal">
+            <input class="govuk-checkboxes__input" id="proposal-7" name="proposal" type="checkbox" value="Replacement of a roof structure and/or covering" v-model="selectedProposal">
             <label class="govuk-label govuk-checkboxes__label" for="proposal-7">
               <strong>Replacement of a roof structure and/or covering</strong>
             </label>
           </div>
 
           <div class="govuk-checkboxes__item">
-            <input class="govuk-checkboxes__input" id="proposal-8" name="proposal" type="checkbox" value="Basement works" v-model="selectedProposal">
+            <input class="govuk-checkboxes__input" id="proposal-8" name="proposal" type="checkbox" value="Removal of chimney" v-model="selectedProposal">
             <label class="govuk-label govuk-checkboxes__label" for="proposal-8">
               <strong>Removal of chimney</strong>
             </label>
           </div>
 
           <div class="govuk-checkboxes__item">
-            <input class="govuk-checkboxes__input" id="proposal-9" name="proposal" type="checkbox" value="Basement works" v-model="selectedProposal">
+            <input class="govuk-checkboxes__input" id="proposal-9" name="proposal" type="checkbox" value="Addition of chimney" v-model="selectedProposal">
             <label class="govuk-label govuk-checkboxes__label" for="proposal-9">
               <strong>Addition of chimney</strong>
             </label>
