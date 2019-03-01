189c792b2a0bc84a4299445b5a5901f6da00aa83
Ana R <ana.rdrg@gmail.com>
2019-03-01 14:45:58 +0000

    If there are selections that need information about floor area, it should go there. Otherwise go to "Trees";

--- planning-app/src/common/navigate.js
+++ planning-app/src/common/navigate.js
@@ -25,9 +25,21 @@ export default {
         element.proposalId ===currentWorks.proposalId
       );
 
-      if (proposalFlow.length === currentWorkIndexInFlow) {
-        //go to trees
-        console.log('----------GO TO TREES');
+      if (proposalFlow.length === currentWorkIndexInFlow + 1) {
+        var needsFloorArea = proposalFlow.find(function(element) {
+          return element.proposalId === 'extension_original_house_single_storey_extension' || 
+          element.proposalId === 'extension_original_house_two_storey_extension' || 
+          element.proposalId === 'extension_original_house_part_single_part_two_storey_extension' ||
+          element.proposalId === 'extension_original_house_outbuilding' ||
+          element.proposalId === 'extension_original_house_porch';
+        });
+
+        if (needsFloorArea) {
+          return {name: 'WorksFloorArea'}
+        } else {
+          return {name: 'Trees'}
+        }
+
       } else {
         return { name: proposalFlow[currentWorkIndexInFlow + 1].goTo[0], params: {currentLevelInfo: proposalFlow[currentWorkIndexInFlow + 1], id: proposalFlow[currentWorkIndexInFlow + 1].proposalId }};
       }
