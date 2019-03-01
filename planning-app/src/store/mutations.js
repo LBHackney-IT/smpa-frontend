7de2ee06e9fa2a072530e59c6a5bba0d14983f1d
Ana R <ana.rdrg@gmail.com>
2019-03-01 16:57:06 +0000

    Don't allow duplicated selections to be added to the flow

--- planning-app/src/store/mutations.js
+++ planning-app/src/store/mutations.js
@@ -23,6 +23,19 @@ export function addAboutDevelopmentAnswers (state, data) {
 }
 
 export function createFirstFlow (state, data) {
+  data.selectedProposal.forEach(function(element, index) {
+    state.state.proposalFlow.forEach(function(el) {
+
+      if (element === el.proposalId) {
+        data.selectedProposal.splice(index, 1);
+      }
+    });
+  });
+
+  if (data.selectedProposal.length === 0) {
+    return;
+  }
+
   var mapKeys = Object.keys(state.state.proposalMap);
 
   var currentLevelInMap = mapKeys.find(function(element) {
@@ -31,44 +44,47 @@ export function createFirstFlow (state, data) {
 
   var listOfOptions = state.state.proposalMap[currentLevelInMap];
 
-  var firstFlow = [];
+  let firstFlow = [];
 
-  listOfOptions.forEach(function(element) {
-    firstFlow = firstFlow.concat(element);
+  data.selectedProposal.forEach(function(element) {
+    var selectedProposal = listOfOptions.find(function(option) {
+      return option.proposalId === element;
+    });
+
+    firstFlow.push(selectedProposal);
+
   });
 
   state.state.proposalFlow = firstFlow;
+
 }
 
 export function updateFlow (state, data) {
+  //check if selected proposal already exists
+  data.selectedProposal.forEach(function(element, index) {
+    state.state.proposalFlow.forEach(function(el) {
 
+      if (element === el.proposalId) {
+        data.selectedProposal.splice(index, 1);
+      }
+    });
+  });
+
+  if (data.selectedProposal.length === 0) {
+    return;
+  }
 
   var mapKeys = Object.keys(state.state.proposalMap);
-
-  console.log('-------CURRENT FLOW', state.state.proposalFlow);
-
-  console.log('----map keys', mapKeys);
 
   var currentLevelInMap = mapKeys.find(function(element) {
     return element === data.currentLevel;
   });
 
-
-
-  console.log('current', currentLevelInMap);
-
   var listOfOptions = state.state.proposalMap[currentLevelInMap];
-
-
-  console.log('------LIST OF OPTIONS', listOfOptions);
-
-  console.log('------data sent', data);
 
   let flow = [];
 
   data.selectedProposal.forEach(function(element) {
-    console.log('element', element);
-    
     var selectedProposal = listOfOptions.find(function(option) {
       return option.proposalId === element;
     });
@@ -77,23 +93,13 @@ export function updateFlow (state, data) {
 
   });
 
-
   var currentLevelPosition = state.state.proposalFlow.findIndex(x => x.proposalId === currentLevelInMap);
-
-
-  console.log('-------new flow? ', flow);
 
   for (let i = 0; i < flow.length; i++) { 
     state.state.proposalFlow.splice(currentLevelPosition + 1 + i, 0, flow[i]);
   }
 
   console.log('--------FINAL FLOW', state.state.proposalFlow);
-
-
-
-
-
-  // state.state.proposalFlow = firstFlow;
 }
 
 
