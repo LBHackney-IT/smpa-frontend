a95d4c541573c02c55e8555877080b1359438392
Ana R <ana.rdrg@gmail.com>
2019-03-12 17:13:18 +0000

    Make all steps expanded by default

--- planning-app/src/views/PlanningApplicationSteps.vue
+++ planning-app/src/views/PlanningApplicationSteps.vue
@@ -21,38 +21,38 @@
   const steps = [{
     title: "Site details",
     name: "SiteDetails",
-    content: '<p>View the geographic information related to your site.</p><a class="govuk-button" href="/sites/search">Find about your site</a>',
-    isActive: false
+    content: '<p>View the geographic information related to your site.</p><a class="govuk-button" href="/sites/search">Start your application</a>',
+    isActive: true
   },
   {
     title: "Description of the works",
     name: "FormWorkDescription",
     content: '<p>Supply relevant information to describe your proposal.</p>',
-    isActive: false
+    isActive: true
   },
   {
     title: "Supporting documentation",
     name: "FormDocumentation",
     content: '<p>Supply documentation to describe your proposal.</p>',
-    isActive: false
+    isActive: true
   },
   {
     title: "Declarations", 
     name: "FormDeclarations",
     content: '<p>Make the necessary legal declarations.</p>',
-    isActive: false
+    isActive: true
   },
   {
     title: "Contact details",
     name: "FormContact",
     content: '<p>Supply your details/details of the applicant so the local authority can contact you. </p>',
-    isActive: false
+    isActive: true
   },
   {
     title: "Pay and submit",
     name: "FormPaySubmit",
     content: '<p>Payment of the correct fee is required at the point of submission. If you don’t supply the correct fee you risk your application being invalid. You can pay for your application as part of this service using credit or debit card. <a target="_blank" href="https://hackney.gov.uk/planning-guidance#Back%20to%20top">Read more information</a> on current planning fees.</p>',
-    isActive: false
+    isActive: true
   }];
 
   export default {
