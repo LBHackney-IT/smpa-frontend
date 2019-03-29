81c49935f641f790d3d1387817a92d9ea341a6b7
Ana R <ana.rdrg@gmail.com>
2019-03-29 12:05:52 +0000

    Add markup for sign in page;

---
+++ planning-app/src/views/SignIn.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="govuk-grid-row">
+    <div class="govuk-grid-column-two-thirds">
+      <h1 class="govuk-heading-xl">Sign in</h1>
+
+      <p>Sign in or <a href="">create an account</a> to get started</p>
+
+      <div class="govuk-form-group">
+        <label class="govuk-label" for="email">
+          Email address
+        </label>
+        <input class="govuk-input govuk-!-width-two-thirds" id="email" name="email" type="email" autocomplete="email" spellcheck="false">
+      </div>
+
+      <div class="govuk-form-group">
+        <label class="govuk-label" for="password">
+          Password
+        </label>
+        <input class="govuk-input govuk-!-width-two-thirds" id="password" name="password" type="password" autocomplete="password" spellcheck="false">
+      </div>
+
+      <v-cta name="Sign in" :onClick="signIn"></v-cta><br>
+
+      <a href="">Forgot your password?</a>
+    </div>
+  </div>
+</template>
+
+<script>
+  import vCta from '../components/Cta.vue';
+
+  export default {
+    name: 'SignIn',
+    components: {
+      vCta
+    },
+    methods: {
+      signIn() {
+        
+      }
+    }
+
+  }
+</script>
