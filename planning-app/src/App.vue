6b8d94b01a7a56a70493d41d40dafd5c864204ab
Ana R <ana.rdrg@gmail.com>
2019-04-12 17:05:04 +0100

    Add "beta" label

--- planning-app/src/App.vue
+++ planning-app/src/App.vue
@@ -2,6 +2,16 @@
   <div id="app" class="govuk-template__body">
     <Header msg="Header"/>
     <div class="govuk-width-container main-content-wrapper">
+      <div class="govuk-phase-banner">
+        <p class="govuk-phase-banner__content"><strong class="govuk-tag govuk-phase-banner__content__tag ">
+        beta
+      </strong>
+          <span class="govuk-phase-banner__text">
+            This is a new service – your
+            <a class="govuk-link" href="#">feedback</a> will help us to improve it.
+          </span>
+        </p>
+      </div>
       <main class="govuk-main-wrapper " id="main-content" role="main">
         <router-view></router-view>
       </main>
