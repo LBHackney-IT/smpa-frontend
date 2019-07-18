151483702bcba5285db24b20c139a4d6bf9ef5a3
Ana R <ana.rdrg@gmail.com>
2019-07-18 11:43:47 +0100

    SMPA-14 #comment Style account actions page and add basic links

--- planning-app/src/views/AccountActions.vue
+++ planning-app/src/views/AccountActions.vue
@@ -1,10 +1,46 @@
 <template>
   <div>
-    <h1>Username</h1>
+    <span class="govuk-caption-xl">Account</span>
+    <h1 class="govuk-heading-xl">Planning Applications</h1>
 
-    <div>apply</div>
-    <div>manage</div>
-    <div>get advice</div>
+    <div class="govuk-grid-row">
+      <div class="govuk-grid-column-one-third">
+        <h2 class="govuk-heading-m">
+          <router-link :to="{ name: 'SiteSearch' }">Apply</router-link>
+        </h2>
+        <p>You can submit and pay online</p>
+      </div>
+
+      <div class="govuk-grid-column-one-third">
+        <h2 class="govuk-heading-m">
+          <router-link :to="{ name: 'UserApplications' }">Manage</router-link>
+        </h2>
+        <p>You can view your planning applications history, edit a draft application or make amendments to an invalid application in order to re-submit.</p>
+      </div>
+
+      <div class="govuk-grid-column-one-third">
+        <h2 class="govuk-heading-m">Get advice</h2>
+        <p>All you need to know about the planning application process and what you have to prepare.</p>
+      </div>
+    </div>
+
+    <h2 class="govuk-heading-l">Get help</h2>
+
+    <p class="govuk-body">Ask the Hackney's Planning Duty Desk about help you can get if you:</p>
+    <ul class="govuk-body">
+      <li>Are you sure if you need planning permission.</li>
+      <li>Know you need planning permission but need some help.</li>
+    </ul>
+
+    <div class="govuk-inset-text">
+      <span class="govuk-!-font-weight-bold">Planning Duty Desk:</span><br>
+
+      Hackney Service Centre, 1 Hillman Street E8 1DY<br>
+      Email: planning@hackney.gov.uk<br>
+      Telephone: 020 8356 8062<br>
+      Monday to Friday: 10am - 12 noon, 2pm - 4pm, in person<br>
+      Monday to Friday: 9am - 5pm, on the phone<br>
+    </div>
   </div>
 </template>
 
